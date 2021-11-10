7b9f83fb9583f94da2cfd307f1054ff7327a1f64
fepal10 <felipepalaciosoviedo@gmail.com>
2021-11-10 16:11:22 -0500

    Se creó carpeta Styles en src

--- src/App.js
+++ src/App.js
@@ -1,27 +1,14 @@
 import logo from './logo.svg';
-import './App.css';
+import './Styles/Styles.css';
 
 function App() {
   return (
     <div className="App">
-      <div class="loginDiv">
-        <form class="form">
-          <p class="loginP">Iniciar sesión</p>
-          <input
-            type="text"
-            name="Usuario"
-            placeholder="Usuario"
-            class="userClass"
-          ></input>
-          <input
-            type="password"
-            name="Contraseña"
-            placeholder="Contraseña"
-            class="passwordClass"
-          ></input>
-          <button type="submit" class="boton1">
-            Entrar
-          </button>
+      <div className="loginDiv">
+        <form className="form">
+          <p className="loginP">Iniciar sesión</p>
+          <input type="text" name="Usuario" placeholder="Usuario" className="userClass"></input>
+          <input type="password" name="Contraseña" placeholder="Contraseña" className="passwordClass"></input>
         </form>
       </div>
     </div>
